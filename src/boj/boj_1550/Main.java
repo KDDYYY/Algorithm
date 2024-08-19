70e8a94f127751479c4c707f1023c4e0fd4593b0
Kim Dong Yeon <do121712@naver.com>
2024-08-19 23:19:24 +0900

    solve

---
+++ src/boj/boj_1550/Main.java
@@ -0,0 +1,10 @@
+package boj.boj_1550;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        System.out.println(Integer.parseInt(a,16));
+    }
+}
