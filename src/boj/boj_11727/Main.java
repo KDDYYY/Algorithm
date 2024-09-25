69ab72ec01a92f61b7c0c30bdd067aba30573f1d
Kim Dong Yeon <do121712@naver.com>
2024-09-26 01:58:37 +0900

    solve

---
+++ src/boj/boj_11727/Main.java
@@ -0,0 +1,23 @@
+package boj.boj_11727;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        System.out.println(size(N));
+    }
+
+    static int size(int N) {
+        if (N == 1) return 1;
+        if (N == 2) return 3;
+        if (dp[N] != 0) return dp[N];
+        return dp[N] = (size(N - 1) + 2 * size(N - 2)) % 10007;
+    }
+}
